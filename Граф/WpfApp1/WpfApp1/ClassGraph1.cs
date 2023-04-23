c70f720259cc6315a5adbdd64bf76ceab25acf9e
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-04-23 21:07:05 +0400

    LOOP

---
+++ Граф/WpfApp1/WpfApp1/ClassGraph1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public class ClassGraph1 : DependencyObject
+    {
+        public ObservableCollection<Node> Nodes { get; private set; }
+        public ObservableCollection<Edge> Edges { get; private set; }
+        public int nextIndex;
+        public ClassGraph1()
+        {
+            this.Nodes = new ObservableCollection<Node>();
+            this.Edges = new ObservableCollection<Edge>();        
+            this.nextIndex = 1;           
+        }
+        public Node AddNode(double x, double y)
+        {
+            Node node = new Node(nextIndex, x, y);
+            this.Nodes.Add(node);
+            nextIndex++;
+            return node;
+        }
+        public void AddEdge(Node startNode, Node endNode)
+        {
+            this.Edges.Add(new Edge(startNode, endNode));
+        }
+        public int[,] BuildAdjacencyMatrix(ObservableCollection<Node> nodes, ObservableCollection<Edge> edges)
+        {
+            // Создаем матрицу размером равным числу узлов
+            int[,] adjacencyMatrix = new int[nodes.Count, nodes.Count];
+            // Заполняем матрицу нулями
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                for (int j = 0; j < nodes.Count; j++)
+                {
+                    adjacencyMatrix[i, j] = 0;
+                }
+            }
+            // Заполняем матрицу единицами в соответствующих ячейках, где есть связь между узлами
+            foreach (Edge edge in edges)
+            {
+                    int startNodeIndex = nodes.IndexOf(edge.StartNode);
+                    int endNodeIndex = nodes.IndexOf(edge.EndNode);
+                    adjacencyMatrix[startNodeIndex, endNodeIndex] = 1;
+                    adjacencyMatrix[endNodeIndex, startNodeIndex] = 1;       
+            }
+            return adjacencyMatrix;
+        }
+
+        public void Clear()
+        {
+            this.Nodes = null;
+            this.Edges = null;
+            this.nextIndex = 1;
+        }
+    }
+    public class Edge : DependencyObject
+    {
+        public double startNode, endNode;
+        public Node StartNode
+        {
+            get { return (Node)GetValue(StartNodeProperty); }
+            set { SetValue(StartNodeProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartNodeProperty =
+            DependencyProperty.Register("StartNode", typeof(Node), typeof(Edge), new PropertyMetadata(null));
+        public Node EndNode
+        {
+            get { return (Node)GetValue(EndNodeProperty); }
+            set { SetValue(EndNodeProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for EndNote.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EndNodeProperty =
+            DependencyProperty.Register("EndNode", typeof(Node), typeof(Edge), new PropertyMetadata(null));
+        public Edge(Node startNode, Node endNode)
+        {
+            this.StartNode = startNode;
+            this.EndNode = endNode;
+        }
+    }
+    public class Node : DependencyObject
+    {
+        public int Index
+        {
+            get { return (int)GetValue(IndexProperty); }
+            set { SetValue(IndexProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Index.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IndexProperty =
+            DependencyProperty.Register("Index", typeof(int), typeof(Node), new PropertyMetadata(0));
+        public double X
+        {
+            get { return (double)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register("X", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+        public double Y
+        {
+            get { return (double)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Y.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register("Y", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+        public double CentreX
+        {
+            get { return (double)GetValue(CentreXProperty); }
+            set { SetValue(CentreXProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for CentrX.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CentreXProperty =
+            DependencyProperty.Register("CentreX", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+        public double CentreY
+        {
+            get { return (double)GetValue(CentreYProperty); }
+            set { SetValue(CentreYProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for CentrY.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CentreYProperty =
+            DependencyProperty.Register("CentreY", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+        public double Size
+        {
+            get { return (double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Size.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register("Size", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+        public Node(int index, double centerX, double centerY)
+        {
+            this.Index = index;
+            this.CentreX = centerX;
+            this.CentreY = centerY;
+            this.Size = 50;
+            this.X = this.CentreX - this.Size / 2;
+            this.Y = this.CentreY - this.Size / 2;
+        }
+    }
+}
---
+++ Граф/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private ClassGraph1 Graph;
+        private Canvas canvasNodes;
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            Graph = new ClassGraph1();
+          
+            ListBoxNodes.ItemsSource = Graph.Nodes;
+            ListBoxEdges.ItemsSource = Graph.Edges;         
+
+        }
+
+        //проверка спауна
+        private void canvasNodes_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Canvas canvasNodes = sender as Canvas;
+                if (canvasNodes != null)
+                {
+                    Point point = e.GetPosition(canvasNodes);
+                    Graph.AddNode(point.X, point.Y);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private bool isDraggingNode = false;
+        private Point? previousPoint = null;
+        private Node firstNode = null;
+
+        List<int> Svyzi = new List<int> { };
+        //спаун связей и узлов
+        private void gridNode_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            if (grid != null)
+            {
+                grid.CaptureMouse();
+
+                if (radioAddNode.IsChecked == true)
+                {
+                    isDraggingNode = true;
+                    previousPoint = e.GetPosition(canvasNodes);
+                }
+
+                if (radioAddEdge.IsChecked == true)
+                {
+                    Svyzi.Add(1);
+                    Node currentNode = null;
+                    currentNode = grid.DataContext as Node;
+                    if (firstNode == null)
+                    {
+                        
+                        firstNode = currentNode;
+                        
+                    }
+                    else
+                    {
+                        Svyzi.Add(0);
+                        Graph.AddEdge(firstNode, currentNode);
+                        firstNode = null;
+                    }
+
+                }
+            }
+        }
+
+        // передвижение точек
+        private void gridNode_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingNode)
+            {
+                Grid element = sender as Grid;
+                if (element != null)
+                {
+                    Node node = element.DataContext as Node;
+                    
+                    if (node != null && previousPoint.HasValue)
+                    {
+                        Point point = e.GetPosition(canvasNodes);
+
+                        double xDiff = point.X - previousPoint.Value.X;
+                        double yDiff = point.Y - previousPoint.Value.Y;
+
+                        node.X += xDiff;
+                        node.Y += yDiff;
+
+                        node.CentreX += xDiff;
+                        node.CentreY += yDiff;
+
+                        previousPoint = point;
+                    }
+                }
+              
+            }
+        }
+
+        private void gridNode_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            if (grid != null)
+            {
+                grid.ReleaseMouseCapture();
+                isDraggingNode = false;
+                previousPoint = null;
+            }
+        }
+
+       private void canvasNodes_Loaded(object sender, RoutedEventArgs e)
+       {
+            canvasNodes = sender as Canvas;
+       }    
+
+
+        // матрица смежности
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            int[,] Table = Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges);
+
+
+            if (Table.GetLength(0) == 0) 
+            {
+                BoxMatrix.Text = string.Empty;
+                BoxMatrix.Background = Brushes.IndianRed;
+                return; 
+            }
+
+            BoxMatrix.Background = Brushes.Transparent;
+
+            List<int> myStrings = new List<int> { };
+
+            for (var i = 0; i < Table.GetLength(0); i++)
+            {
+                for (var j = 0; j < Table.GetLength(1); j++)
+                {
+                    myStrings.Add(Table[i, j]);
+                }
+            }
+
+            int uzel = 1, uzel_uzel = 1, end = (Graph.nextIndex - 1) * 2, start = 0;
+            string matrix = string.Empty, uzel1point = string.Empty, myIntsString = string.Join(" ", myStrings) + " ";
+         
+            uzel1point += "   | ";
+            for (var i = 0; i < Graph.nextIndex - 1; i++)
+            {               
+                uzel1point += $"{uzel_uzel} ";
+                uzel_uzel++;
+            }
+
+            for (var i = 0; i < Graph.nextIndex - 1; i++)
+            {
+                matrix += $"{uzel} | " + myIntsString.Substring(start, end) + "\n";
+                start += end;
+                uzel++;
+            }
+            uzel1point += "\n";
+
+            BoxMatrix.Text = uzel1point + matrix;
+           
+        }
+
+        // максимальный поток
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+          if(Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges).GetLength(0) != 0)
+          {
+                FlowBox.Background = Brushes.Transparent;
+                FlowBox.Text = $"От 1 до {Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges).GetLength(0)} узла\nРавен: " + Convert.ToString(MaxFlow(Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges), 0, Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges).GetLength(0) - 1));
+          }
+            else
+            {
+                FlowBox.Text = string.Empty;
+                FlowBox.Background = Brushes.IndianRed;
+            }   
+          
+        }
+
+        private int MaxFlow(int[,] graph, int source, int sink)
+       {
+            int[] parent = new int[graph.GetLength(0)];
+            int maxFlow = 0;
+            while (Bfs(graph, source, sink, parent))
+            {
+                int pathFlow = int.MaxValue;
+                for (int v = sink; v != source; v = parent[v])
+                {
+                    int u = parent[v];
+                    pathFlow = Math.Min(pathFlow, graph[u, v]);
+                }
+                for (int v = sink; v != source; v = parent[v])
+                {
+                    int u = parent[v];
+                    graph[u, v] -= pathFlow;
+                    graph[v, u] += pathFlow;
+                }
+                maxFlow += pathFlow;
+            }
+            return maxFlow;
+       }
+
+       private  bool Bfs(int[,] graph, int source, int sink, int[] parent)
+       {
+            bool[] visited = new bool[graph.GetLength(0)];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(source);
+            visited[source] = true;
+            parent[source] = -1;
+            while (queue.Count > 0)
+            {
+                int u = queue.Dequeue();
+                for (int v = 0; v < graph.GetLength(0); v++)
+                {
+                    if (!visited[v] && graph[u, v] > 0)
+                    {
+                        queue.Enqueue(v);
+                        visited[v] = true;
+                        parent[v] = u;
+                    }
+                }
+            }
+            return visited[sink];
+       }
+
+
+        //алгоритм для обхода в глубь графа
+
+        private bool[] visited = new bool[1000];
+         
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+             if(Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges).GetLength(0) != 0)
+            {
+                listBox1.Background = Brushes.Transparent;
+                for (int i = 0; i < visited.Length; i++)
+                {
+                    visited[i] = false;
+                }
+                listBox1.Items.Clear();
+                DFS(0);
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Background = Brushes.IndianRed;
+            }
+
+        }
+
+        private void DFS(int i)
+        {
+            int[,] adjMatrix = Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges);
+            visited[i] = true;
+            listBox1.Items.Add("Узел: " + (i+1).ToString());
+            for (int j = 0; j < adjMatrix.GetLength(1); j++)
+            {
+                if (adjMatrix[i, j] == 1 && !visited[j])
+                {
+                    DFS(j);
+                }
+            }
+        }
+
+
+        // Определение циклов
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            int[,] adjacencyMatrix = Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges);
+
+
+
+            BoxCycle.Text = string.Empty;
+
+
+            if (Graph.BuildAdjacencyMatrix(Graph.Nodes, Graph.Edges).GetLength(0) != 0)
+            {
+                BoxCycle.Background = Brushes.Transparent;
+
+                // Эйлеров цикл
+                bool euler = true;
+                for (int i = 0; i < adjacencyMatrix.GetLength(0); i++)
+                {
+                    int degree = 0;
+                    for (int j = 0; j < adjacencyMatrix.GetLength(1); j++)
+                    {
+                        degree += adjacencyMatrix[i, j];
+                    }
+                    if (degree % 2 != 0)
+                    {
+                        euler = false;
+                        break;
+                    }
+                }
+
+                if (euler)
+                {
+                    BoxCycle.Text += "Граф содержит \nэйлеров цикл.\n\n";
+                }
+                else
+                {
+                    BoxCycle.Text += "Граф не содержит \nэйлеров цикл.\n\n";
+                }
+
+
+                // Гамлетов цикл
+
+                if (HamiltonianCycle(adjacencyMatrix))
+                {
+                    BoxCycle.Text += "Граф содержит \nгамильтонов цикл.\n\n";
+                }
+                else
+                {
+                    BoxCycle.Text += "Граф не содержит \nгамильтонов цикл.\n\n";
+                }
+            }
+            else
+            {
+                BoxCycle.Text = string.Empty;
+                BoxCycle.Background = Brushes.IndianRed;
+            } 
+
+        }
+
+         bool HamiltonianCycle(int[,] graph)
+         {
+            int[] path = new int[graph.GetLength(0)];
+            for (int i = 0; i < path.Length; i++)
+            {
+                path[i] = -1;
+            }
+            path[0] = 0;
+            if (!HamiltonianCycleUtil(graph, path, 1))
+            {
+                return false;
+            }
+            return true;
+         }
+
+         bool HamiltonianCycleUtil(int[,] graph, int[] path, int pos)
+         {
+            if (pos == graph.GetLength(0))
+            {
+                if (graph[path[pos - 1], path[0]] == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            for (int v = 1; v < graph.GetLength(0); v++)
+            {
+                if (IsSafe(v, graph, path, pos))
+                {
+                    path[pos] = v;
+                    if (HamiltonianCycleUtil(graph, path, pos + 1))
+                    {
+                        return true;
+                    }
+                    path[pos] = -1;
+                }
+            }
+            return false;
+         }
+
+        static bool IsSafe(int v, int[,] graph, int[] path, int pos)
+        {
+            if (graph[path[pos - 1], v] == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < pos; i++)
+            {
+                if (path[i] == v)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        // очистка
+        void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Graph.Nodes.Clear();
+            Graph.Edges.Clear();
+            BoxCycle.Text = string.Empty;
+            FlowBox.Text = string.Empty;
+            BoxMatrix.Text = string.Empty;
+            listBox1.Items.Clear();
+
+            BoxMatrix.Background = Brushes.Transparent;
+            FlowBox.Background = Brushes.Transparent;
+            BoxCycle.Background = Brushes.Transparent;
+            listBox1.Background = Brushes.Transparent;
+
+            Graph = null;
+
+            InitializeComponent();
+            Graph = new ClassGraph1();
+            ListBoxNodes.ItemsSource = Graph.Nodes;
+            ListBoxEdges.ItemsSource = Graph.Edges;
+
+        }
+    }
+
+}
